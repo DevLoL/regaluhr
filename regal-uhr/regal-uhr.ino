89aebc07cdfdf14deb8f1685674c096f665354fd
Thomas R. Koll <tomk32@tomk32.de>
2017-01-15 19:24:59 +0100

    Add led strip showing hours, minutes and seconds in short colour bursts shifting the hue over the period of a minute, hour and day

--- regal-uhr/regal-uhr.ino
+++ regal-uhr/regal-uhr.ino
@@ -33,6 +33,18 @@ static time_t local_time;
 
 Adafruit_7segment matrix = Adafruit_7segment();
 
+#include "FastLED.h"
+
+// How many leds in your strip?
+#define NUM_LEDS 60
+#define FASTLED_ESP8266_RAW_PIN_ORDER
+#define DATA_PIN D6
+
+// Define the array of leds
+CRGB leds[NUM_LEDS];
+
+#define LED_SPEED 10 // steps per second
+
 void connectWifi() {
     // We start by connecting to a WiFi network
   Serial.print("Connecting to ");
@@ -58,6 +70,32 @@ void displayTimeOn7Segment() {
   matrix.writeDisplay();
 }
 
+void setLEDcolor(int pos, CHSV color, int num_leds) {
+  for (int i; i < num_leds; i++) {
+    leds[(pos + i) % NUM_LEDS] = color;
+  }
+}
+void displayTimeOnLEDs() {
+  static int pos = 0;
+  static int pos_20 = 20;
+  static int pos_40 = 40;
+  // show leds for second, minute and hour, send them around the strip
+  setLEDcolor(pos, CHSV(second(local_time) * 4.25, 255, 255), 5);
+  setLEDcolor(pos_40, CHSV(minute(local_time) * 4.25, 255, 255), 3);
+  setLEDcolor(pos_20, CHSV(hour(local_time) * 10.625, 255, 255), 2);
+  FastLED.show();
+
+  // Only turn off the last LED, the rest was overwritten anyways
+  leds[pos] = CRGB::Black;
+  leds[pos_20] = CRGB::Black;
+  leds[pos_40] = CRGB::Black;
+  FastLED.show();
+
+  pos = (pos + 1) % NUM_LEDS;
+  pos_20 = (pos_20 + 1) % NUM_LEDS;
+  pos_40 = (pos_40 + 1) % NUM_LEDS;
+}
+
 void loop() {
   if (WiFi.status() != WL_CONNECTED) {
     connectWifi();
@@ -67,7 +105,12 @@ void loop() {
   local_time = timezone.toLocal(timeClient.getEpochTime());
 
   displayTimeOn7Segment();
-  
+
+  static unsigned long next_led_update = 0;
+  if (next_led_update < millis()) {
+    next_led_update = millis() + (LED_SPEED / 1000);
+    displayTimeOnLEDs();
+  }
 }
 
 void setup() {
@@ -79,4 +122,6 @@ void setup() {
   
   timeClient.begin();
 
+  FastLED.addLeds<WS2812, DATA_PIN, GRB>(leds, NUM_LEDS);
 }
+
